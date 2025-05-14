8c8ea955315a4f60de53500781739ec98416da81
easton-honaker <143205545+easton-honaker@users.noreply.github.com>
2025-05-14 00:12:58 -0400

    Initial commit

---
+++ tests/conftest.py
@@ -0,0 +1,45 @@
+import os
+import pytest
+
+from astrodbkit.astrodb import Database, create_database
+
+
+REFERENCE_TABLES = [
+    "Publications",
+    "Telescopes",
+    "Instruments",
+    "PhotometryFilters",
+    "Versions",
+    "Regimes",
+    "AssociationList",
+    "SourceTypeList",
+    "ParameterList",
+    "CompanionList",
+]
+DB_PATH = "data"
+DB_NAME = "tests/astrodb_template_tests.sqlite"
+SCHEMA_PATH = "schema/schema.yaml"
+CONNECTION_STRING = "sqlite:///" + DB_NAME
+
+
+# Create a fresh template database for the data and integrity tests
+@pytest.fixture(scope="session", autouse=True)
+def db():
+
+    # Confirm the schema yaml file is present
+    assert os.path.exists(SCHEMA_PATH)
+
+    # Remove any existing copy of the test database
+    if os.path.exists(DB_NAME):
+        os.remove(DB_NAME)
+        assert not os.path.exists(DB_NAME)
+
+    # Create the database using the Felis schema
+    create_database(CONNECTION_STRING, felis_schema=SCHEMA_PATH)
+    assert os.path.exists(DB_NAME)
+
+    # Connect and load to the database
+    db = Database(CONNECTION_STRING, reference_tables=REFERENCE_TABLES)
+    db.load_database(DB_PATH, verbose=False)
+
+    return db
