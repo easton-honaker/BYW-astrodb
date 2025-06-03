a4c5363635936905131465a455e8a40bc7593a8a
Easton Honaker <ehonaker@udel.edu>
2025-06-03 15:36:26 -0400

    first20objects
    csv file and ingest loop script

--- scripts/ingest_BYWsample.py
+++ scripts/ingest_BYWsample.py
@@ -4,12 +4,14 @@ from astrodb_utils import load_astrodb
 from astrodb_utils.sources import ingest_source, ingest_name
 from astrodb_utils.publications import ingest_publication
 from astrodb_utils.photometry import ingest_photometry
+import astropy
+from astropy.table import Table
 
 # Load the database
 db_file = "BYW_HON.sqlite"
 felis_schema = "schema/schema.yaml"
 
-DB_SAVE = True
+DB_SAVE = False
 DB_RECREATE = True
 
 reference_tables = [
@@ -31,14 +33,14 @@ db = load_astrodb(
 )
 
 
-# unlike single source, let's loop by table
+# unlike single source, let's loop by table.
+
+# first check if we have the relevant publications. for now, manually check the two that are important for the sources
 
 def ingest_BYWsample_pubs(db):
     #check if hona26 and goodstuff already in db. add if not
     exists_hona26 = find_publication(db, "HONA26")
-    if exists_hona26 == True:
-        continue
-    elif exists_hona26 == False:
+    if exists_hona26 == False:
         ingest_publication(
             db,
             description="HONAKER BYW sample",
@@ -47,26 +49,43 @@ def ingest_BYWsample_pubs(db):
         )
 
     exists_goodstuff = find_publication(db, "Goodstuff")
-    if exists_goodstuff == True:
-        continue
-    elif exists_goodstuff == False:
+    if exists_goodstuff == False:
         ingest_publication(db, 
                            reference="Goodstuff", 
                            description="BYW goodstuff sheet",
                            ignore_ads=True)
 
-def ingest_BYWsample_sources(db):
+def ingest_BYWsample_source(db):
     #loop over all sources and ingest their source and name
     ingest_source(db,
-                  source = ,
-                  ra =,
-                  dec = ,
+                  source = source_name,
+                  ra =source_ra,
+                  dec = source_dec,
                   equinox = "J2000",
-                  comment = "CatWISE2020"
+                  comment = "CatWISE2020",
+                  reference= "HONA26",
+                  raise_error=False,
                   )
     
     ingest_name(db,
-                source = ,
-                other_name =
+                source = source_name,
+                other_name = shortname
                 )
     
+#read in the partial dataset (first 20)
+BYWsubsample = Table.read('BYWfirst20.csv',format='csv')
+
+# Loop through the subsample and ingest each source
+for entry in BYWsubsample:
+    source_name = 'CWISE '+str(entry['Jname'])
+
+    source_ra = entry['catWISE RA']
+    source_dec = entry['catWISE Dec']
+    shortname = entry['shortname']
+    
+    # Ingest the source and name
+    ingest_BYWsample_source(db)
+
+
+if DB_SAVE:
+    db.save_database("data/")
